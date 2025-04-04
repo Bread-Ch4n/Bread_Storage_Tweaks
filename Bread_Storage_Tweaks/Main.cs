737148d7d899167db075526684799e53806cca3e
Bread-Ch4n <94946192+Bread-Ch4n@users.noreply.github.com>
2025-04-04 14:13:20 +0300

    1.0.2
    Remove description so it doesn't repeat a lot.
    Added ability to change slot and row amount for the dealers.

--- Bread_Storage_Tweaks/Main.cs
+++ Bread_Storage_Tweaks/Main.cs
@@ -1,5 +1,9 @@
 ﻿using HarmonyLib;
+using Il2CppScheduleOne.Economy;
+using Il2CppScheduleOne.ItemFramework;
+using Il2CppScheduleOne.NPCs;
 using Il2CppScheduleOne.Storage;
+using Il2CppScheduleOne.UI;
 using MelonLoader;
 using MelonLoader.Utils;
 
@@ -9,14 +13,19 @@ public class Main : MelonMod
 {
     private static readonly string PreferencePath = Path.Combine(MelonEnvironment.UserDataDirectory, "Bread_Storage_Tweaks");
     private static MelonPreferences_Category? _storageCategory;
+
     private static MelonPreferences_Entry<int>? _storageBriefcaseSlotAmount;
     private static MelonPreferences_Entry<int>? _storageLargeStorageRackSlotAmount;
     private static MelonPreferences_Entry<int>? _storageMediumStorageRackSlotAmount;
     private static MelonPreferences_Entry<int>? _storageSmallStorageRackSlotAmount;
+    private static MelonPreferences_Entry<int>? _storageDealerSlotAmount;
+
     private static MelonPreferences_Entry<int>? _storageBriefcaseRowAmount;
     private static MelonPreferences_Entry<int>? _storageLargeStorageRackRowAmount;
     private static MelonPreferences_Entry<int>? _storageMediumStorageRackRowAmount;
     private static MelonPreferences_Entry<int>? _storageSmallStorageRackRowAmount;
+    private static MelonPreferences_Entry<int>? _storageDealerRowAmount;
+
     public override void OnInitializeMelon()
     {
         Directory.CreateDirectory(PreferencePath);
@@ -24,32 +33,26 @@ public class Main : MelonMod
         _storageCategory = MelonPreferences.CreateCategory("Storage Tweaks");
         _storageCategory.SetFilePath(Path.Combine(PreferencePath, "Storage.cfg"));
 
-        _storageBriefcaseSlotAmount = _storageCategory.CreateEntry("Briefcase Inventory Slot Amount", 4,
-            description:
-            "[0-20] Do not make this smaller or remove the mod. You will loose the items from the extra slots.");
-        _storageLargeStorageRackSlotAmount = _storageCategory.CreateEntry("Large Storage Rack Inventory Slot Amount", 8,
-            description:
-            "[0-20] Do not make this smaller or remove the mod. You will loose the items from the extra slots.");
+        _storageBriefcaseSlotAmount = _storageCategory.CreateEntry("Briefcase Inventory Slot Amount", 4, description:"Do not make any values smaller or remove the mod. You will loose the items from the extra slots.\nThe max slot value is 20!");
+        _storageLargeStorageRackSlotAmount = _storageCategory.CreateEntry("Large Storage Rack Inventory Slot Amount", 8);
         _storageMediumStorageRackSlotAmount = _storageCategory.CreateEntry("Medium Storage Rack Inventory Slot Amount",
-            6,
-            description:
-            "[0-20] Do not make this smaller or remove the mod. You will loose the items from the extra slots.");
-        _storageSmallStorageRackSlotAmount = _storageCategory.CreateEntry("Small Storage Rack Inventory Slot Amount", 4,
-            description:
-            "[0-20] Do not make this smaller or remove the mod. You will loose the items from the extra slots.");
+            6);
+        _storageSmallStorageRackSlotAmount = _storageCategory.CreateEntry("Small Storage Rack Inventory Slot Amount", 4);
+        _storageDealerSlotAmount = _storageCategory.CreateEntry("Dealer Inventory Slot Amount", 5);
+
         if (_storageBriefcaseSlotAmount.Value > 20) _storageBriefcaseSlotAmount.Value = 20;
         if (_storageLargeStorageRackSlotAmount.Value > 20) _storageLargeStorageRackSlotAmount.Value = 20;
         if (_storageMediumStorageRackSlotAmount.Value > 20) _storageMediumStorageRackSlotAmount.Value = 20;
         if (_storageSmallStorageRackSlotAmount.Value > 20) _storageSmallStorageRackSlotAmount.Value = 20;
+        if (_storageDealerSlotAmount.Value > 20) _storageDealerSlotAmount.Value = 20;
 
-        _storageBriefcaseRowAmount = _storageCategory.CreateEntry("Briefcase Inventory Row Amount", 1,
-            description:
-            "The amount of rows for the storage slots");
+        _storageBriefcaseRowAmount = _storageCategory.CreateEntry("Briefcase Inventory Row Amount", 1);
         _storageLargeStorageRackRowAmount = _storageCategory.CreateEntry("Large Storage Rack Inventory Row Amount", 2);
         _storageMediumStorageRackRowAmount = _storageCategory.CreateEntry("Medium Storage Rack Inventory Row Amount",
             2
             );
         _storageSmallStorageRackRowAmount = _storageCategory.CreateEntry("Small Storage Rack Inventory Row Amount", 2);
+        _storageDealerRowAmount = _storageCategory.CreateEntry("Dealer Inventory Row Amount", 1);
 
         var h = new HarmonyLib.Harmony("Bread_Storage_Tweaks");
 
@@ -85,5 +88,27 @@ public class Main : MelonMod
                 _ => __instance.SlotCount
             };
         }
+
+        [HarmonyPatch(typeof(StorageMenu), "Open", typeof(IItemSlotOwner), typeof(string), typeof(string))]
+        [HarmonyPrefix]
+        private static bool DealerInvRowPatch(StorageMenu __instance, IItemSlotOwner owner, string title, string subtitle)
+        {
+            var npcInventory = owner.TryCast<NPCInventory>();
+            if (npcInventory == null || npcInventory.GetComponentInParent<Dealer>() == null) return true;
+
+            __instance.IsOpen = true;
+            __instance.OpenedStorageEntity = null;
+            __instance.SlotGridLayout.constraintCount = _storageDealerRowAmount!.Value;
+            __instance.Open(title, subtitle, owner);
+
+            return false;
+        }
+
+        [HarmonyPatch(typeof(NPCInventory), "Awake")]
+        [HarmonyPrefix]
+        private static void DealerInvPatch(NPCInventory __instance) => _ = __instance.GetComponentInParent<Dealer>()
+            ? __instance.SlotCount = _storageDealerSlotAmount!.Value
+            : (object)null!;
+
     }
 }
